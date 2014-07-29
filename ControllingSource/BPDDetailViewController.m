ea2284a6fc861ae58b5a353885b699cc02612bd8
campelopaulo <prjcampelo@gmail.com>
2014-07-29 17:35:35 -0300

    Alteração do background

--- ControllingSource/BPDDetailViewController.m
+++ ControllingSource/BPDDetailViewController.m
@@ -39,6 +39,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
     [self configureView];
 }
 
